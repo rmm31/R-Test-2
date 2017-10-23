7cb8ad543587dff66a69a7817b7d46eb6225c072
rmm31 <31975834+rmm31@users.noreply.github.com>
2017-10-23 17:23:51 +1100

    Update

--- Boxplots violinplots code.R
+++ Boxplots violinplots code.R
@@ -6,10 +6,26 @@ qplot(y=existingcp, x= 1, geom = "violin") +
   theme(panel.grid.minor.x = element_blank(), panel.grid.minor.y =  element_blank()) +
   theme(axis.title.x = element_blank())
  
-
+#following code needs year changed to factor using as.factor, just putting factor(year) alters the original data
+#this doesn't happen with ggplot code below
 qplot(year, price, data = fin_all_summ_subset2, geom = "boxplot") +
   scale_y_continuous(breaks = seq(0,900, by = 25)) +
   theme_bw() +
   theme(panel.grid.major.x = element_blank(), panel.grid.major.y =  element_blank()) +
   theme(panel.grid.minor.x = element_blank(), panel.grid.minor.y =  element_blank()) 
  # theme(axis.text.x = element_text(colour = 'red', angle = 90)
+
+ggplot(fin_all_summ_subset2, aes(x = factor(year), y = price)) + 
+  geom_boxplot(width = 0.2) +
+  scale_y_continuous(breaks = seq(0,900, by = 25)) +
+  theme_bw() +
+  theme(panel.grid.major.x = element_blank(), panel.grid.major.y =  element_blank()) +
+  theme(panel.grid.minor.x = element_blank(), panel.grid.minor.y =  element_blank()) 
+
+ggplot(fin_all_summ_subset2, aes(x = factor(year), y = price)) + 
+  geom_violin() + 
+  geom_boxplot(width = 0.1) +
+  scale_y_continuous(breaks = seq(0,900, by = 25)) +
+  theme_bw() +
+  theme(panel.grid.major.x = element_blank(), panel.grid.major.y =  element_blank()) +
+  theme(panel.grid.minor.x = element_blank(), panel.grid.minor.y =  element_blank()) 
