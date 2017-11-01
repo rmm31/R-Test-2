50ba4dbce20c0af461070c282bc2d5146eb43ced
rmm31 <31975834+rmm31@users.noreply.github.com>
2017-11-02 08:33:56 +1100

    Adding some additional notes to code, no major changes

--- Code for final numbers report.R
+++ Code for final numbers report.R
@@ -52,10 +52,9 @@ numbers_report_fin_Market_GDP_1_with_existing$model_existing <- c("modelled")
 
 numbers_report_fin_Market_GDP_1_with_existing <- rbind(numbers_report_carbon_pricing_init_no_pilots_r, numbers_report_fin_Market_GDP_1_with_existing)
 
-#create chart
+#create chart with existing carbon prices and modelled carbon prices
 
-numbers_report_new_boxplot <-ggplot(numbers_report_fin_Market_GDP_1_with_existing, aes(x = factor(year), y = price, color = model_existing)) + 
-  #geom_violin() + 
+ggplot(numbers_report_fin_Market_GDP_1_with_existing, aes(x = factor(year), y = price, color = model_existing)) + 
   geom_boxplot(width = 0.3) +
   scale_y_continuous(breaks = seq(0,900, by = 25)) +
   labs(x = "Year", y = bquote('AUD/t'~CO[2]~'or /t'~CO[2]~'-e')) +
@@ -63,7 +62,9 @@ numbers_report_new_boxplot <-ggplot(numbers_report_fin_Market_GDP_1_with_existin
   theme(panel.grid.major.x = element_blank() ) +
   theme(panel.grid.minor.x = element_blank()) +
   theme(axis.text=element_text(size=12),axis.title=element_text(size=14,face="bold")) +
-  scale_color_manual(name = "Carbon value source", labels = c("Existing carbon price", "Modelled carbon value"), values = c("blue", "red", "green", "purple", "brown", "black"))
+  scale_color_manual(name = "Carbon value source", labels = c("Existing carbon prices", "Modelled carbon values"), values = c("blue", "red")) +
+  ggtitle("Existing carbon prices and modelled values") +
+  theme(plot.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=24, hjust=0.5))
 
 #Add US EPA SCC values to dataset
 numbers_report_USEPA_SCC <- as.tibble(numbers_report_USEPA_SCC)
@@ -75,7 +76,8 @@ numbers_report_USEPA_SCC$year <- as.numeric(numbers_report_USEPA_SCC$year)
 library(stringr)
 str_sub(numbers_report_USEPA_SCC$scenario, 1,0) <- "z "
 
-#Add geom_point plot with USEPA SCC values to boxplot
+#Add geom_point plot with USEPA SCC values to boxplots of existing carbon prices and modelled carbon prices, note that generating two legends
+#requires separating the grouping aesthetics so that the boxplots group on colour and the points group on fill
 
 ggplot(numbers_report_fin_Market_GDP_1_with_existing, aes(x = factor(year), y = price)) + 
   geom_boxplot(fill = NA, size = 0.75, width = 0.3, aes(color = model_existing)) +
@@ -102,7 +104,9 @@ ggplot(numbers_report_fin_Market_GDP_1_with_existing, aes(x = factor(year), y = 
   theme(panel.grid.major.x = element_blank() ) +
   theme(panel.grid.minor.x = element_blank()) +
   theme(axis.text=element_text(size=12),axis.title=element_text(size=14,face="bold"), legend.key = element_blank()) +
-  theme(strip.text.x = element_text(size = 14))
+  theme(strip.text.x = element_text(size = 14)) +
+  ggtitle("Existing carbon prices and modelled values") +
+  theme(plot.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=24, hjust=0.5))
 
 facet_names_fin_report <- c(
   `Existing carbon price` = "Existing carbon pricing initiatives", 
@@ -118,7 +122,22 @@ geom_point(data = numbers_report_USEPA_SCC, aes(x = factor(year), y = price, fil
   theme_bw() +
   theme(panel.grid.major.x = element_blank() ) +
   theme(panel.grid.minor.x = element_blank()) +
-  theme(axis.text=element_text(size=12),axis.title=element_text(size=14,face="bold"), legend.key = element_blank()) 
+  theme(axis.text=element_text(size=12),axis.title=element_text(size=14,face="bold"), legend.key = element_blank()) +
+  ggtitle("US EPA Social Cost of Carbon Values") +
+  theme(plot.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=24, hjust=0.5))
+
+#Create points alone for US EPA SCC - coerced scale
+ggplot(numbers_report_fin_Market_GDP_1_with_existing, aes(x = factor(year), y = price)) +
+  geom_point(data = numbers_report_USEPA_SCC, aes(x = factor(year), y = price, fill = scenario), shape = 24, size = 3) +
+  scale_y_continuous(limits = c(0,625), breaks = seq(0,625, by = 25)) +
+  labs(x = "Year", y = bquote(~AUD[2016]~'/t'~CO[2]~' ')) +
+  scale_fill_manual(name = "US EPA SCC values", labels = c("2.5% DR", "3% DR", "5% DR", "95th pct 3% DR"), values = c("green", "purple", "brown", NA)) +
+  theme_bw() +
+  theme(panel.grid.major.x = element_blank() ) +
+  theme(panel.grid.minor.x = element_blank()) +
+  theme(axis.text=element_text(size=12),axis.title=element_text(size=14,face="bold"), legend.key = element_blank()) +
+  ggtitle("US EPA Social Cost of Carbon Values") +
+  theme(plot.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=24, hjust=0.5))
 
 #Create separate boxplots
 
@@ -131,9 +150,12 @@ ggplot(numbers_report_fin_Market_GDP_1, aes(x = factor(year), y = price)) +
   theme_bw() +
   theme(panel.grid.major.x = element_blank() ) +
   theme(panel.grid.minor.x = element_blank()) +
-  theme(axis.text=element_text(size=12),axis.title=element_text(size=14,face="bold"))
+  theme(axis.text=element_text(size=12),axis.title=element_text(size=14,face="bold")) +
+  labs(title = "Modelled Carbon Values", subtitle = "(excluding US EPA SCC values)") +
+  theme(plot.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=24, hjust=0.5)) +
+  theme(plot.subtitle = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=16, hjust=0.5)) 
 
-#Code for all numbers from World Bank existing carbon price (new boxplot)
+#Code for all numbers from World Bank existing carbon price (new boxplot) (coerced scale on request of DELWP)
 
 ggplot(numbers_report_carbon_pricing_init_no_pilots_r, aes(x = factor(year), y = price)) + 
   geom_boxplot(width = 0.1) +
@@ -142,5 +164,22 @@ ggplot(numbers_report_carbon_pricing_init_no_pilots_r, aes(x = factor(year), y =
   theme_bw() +
   theme(panel.grid.major.x = element_blank() ) +
   theme(panel.grid.minor.x = element_blank()) +
-  theme(axis.text=element_text(size=12),axis.title=element_text(size=14,face="bold"))
+  theme(axis.text=element_text(size=12),axis.title=element_text(size=14,face="bold")) +
+  labs(title = "Existing carbon pricing initiatives", subtitle = "(World Bank data)") +
+  theme(plot.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=24, hjust=0.5)) +
+  theme(plot.subtitle = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=16, hjust=0.5)) 
+
+#Code for all numbers from World Bank existing carbon price (new boxplot) (coerced scale on request of DELWP)
+
+ggplot(numbers_report_carbon_pricing_init_no_pilots_r, aes(x = factor(year), y = price)) + 
+  geom_boxplot(width = 0.1) +
+  scale_y_continuous(breaks = seq(0,250, by = 25)) +
+  labs(x = "Year", y = bquote('AUD/t'~CO[2]~'or /t'~CO[2]~'-e')) +
+  theme_bw() +
+  theme(panel.grid.major.x = element_blank() ) +
+  theme(panel.grid.minor.x = element_blank()) +
+  theme(axis.text=element_text(size=12),axis.title=element_text(size=14,face="bold")) +
+  labs(title = "Existing carbon pricing initiatives", subtitle = "(World Bank data)") +
+  theme(plot.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=24, hjust=0.5)) +
+  theme(plot.subtitle = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=16, hjust=0.5)) 
   
